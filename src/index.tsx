e07ff451e3b29a2ff57975d6c40c7f6194c83194
HsarkutH <sarkut.huseyin@gmail.com>
2025-06-10 02:50:49 +0300

    Last touch
    Completed the project

--- src/index.tsx
+++ src/index.tsx
@@ -7,5 +7,6 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
     <App />
+    <title>Select Skip</title>
   </React.StrictMode>
 );
